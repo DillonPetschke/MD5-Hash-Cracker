3feafcb1f6af1c91ac69140f47a35c238e4250da
DillonPetschke <51690971+DillonPetschke@users.noreply.github.com>
2023-04-05 23:28:11 -0500

    Update MD5-Hash-Cracker.sh

--- MD5-Hash-Cracker.sh
+++ MD5-Hash-Cracker.sh
@@ -2,8 +2,8 @@
 #
 #
 # This program reads a txt file named file_1.txt that contains password hashes. 
-# Using md5sum, generating and comparing hashes based on a given worked list (rockyoutop1000.txt)
-# matching results will be displayed to the screen.
+# Using md5sum, generating and comparing hashes based on a given word list (rockyoutop1000.txt)
+# All matching results will be displayed to standard output.
 #
 # Dillon Petschke 4/5/2023
 #
